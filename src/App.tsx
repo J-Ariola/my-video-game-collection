63b044893da84a23659f57c5f5a44542fa800deb
Jarrod Ariola <jarrod.ariola@gmail.com>
2024-05-10 23:43:23 +0900

    feat: display generic header bar

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,29 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from 'react';
+import reactLogo from './assets/react.svg';
+import viteLogo from '/vite.svg';
+import './App.css';
+import Header from './components/Header';
+
+type Props = {
+  title:string;
+  onClick():void;
+}
+
+const ClickCountButton = ({ title, onClick }: Props): React.JSX.Element => {
+  return (
+    <button onClick={onClick}>
+    {title}
+    </button>
+  )
+}
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <div>
-      <h1>My Video Game Collection</h1>
+      <Header/>
+      <ClickCountButton title={"Click button: " + count} onClick={() => setCount(count + 1)}/>
     </div>
   )
 }

77833af9badd3c20f783529a154eca0b5646de9a
Jarrod Ariola <jarrod.ariola@gmail.com>
2024-07-19 11:24:33 +0900

    feat: display game details on /my-games

--- src/MyGames.tsx
+++ src/MyGames.tsx
@@ -1,9 +1,10 @@
 import { useEffect, useState } from 'react';
 import Header from './components/Header';
-import {Box} from '@mui/material'
+import { Box, Container, Grid } from '@mui/material';
 import { GameDetails, GameEntry } from './globals';
 import { auth } from './config/firebaseConfig';
 import { onAuthStateChanged } from 'firebase/auth';
+import GameCardSmall from './components/GameCardSmall';
 
 const BASE_URL: string | null = import.meta.env.VITE_BASE_URL;
 
@@ -24,6 +25,12 @@ function MyGames():React.JSX.Element {
 
     handleUserGameDetails();
   },[userEntries]);
+
+  useEffect (() => {
+    if (!userGameDetails) return;
+
+    console.log(userGameDetails);
+  }, [userGameDetails]);
 
   async function handleUserEntries() {
     try {
@@ -49,15 +56,52 @@ function MyGames():React.JSX.Element {
   async function handleUserGameDetails() {
     try{
       //TODO: Get game details and merge status
+      await Promise.all( userEntries.map( entry => {
+        return fetch(`${BASE_URL}/game-details/${entry.guid}`, {
+          method: "GET"
+        });
+      }))
+      .then( responses => {
+        return Promise.all(responses.map( res => res.json() as unknown as GameDetails));
+      })
+      .then( gameData => {
+        return setUserGameDetails(gameData.sort( (a, b) => {
+          const gameNameA = a.name.toUpperCase();
+          const gameNameB = b.name.toUpperCase();
+          if (gameNameA < gameNameB) return -1;
+          if (gameNameA > gameNameB) return 1;
+          return 0;
+        }));
+      })
     } catch(e) {
       console.error(e);
     }
+  }
+
+  function GetGameStatus(guid: string, userEntries: Array<GameEntry>):number {
+    const index = userEntries.findIndex( entry => entry.guid === guid);
+    return userEntries[index].status | 0;
+  }
+
+  function handleDisplayCards(): React.JSX.Element {
+    return (
+      <Container sx={{marginTop: "40px"}}>
+        <Grid container spacing={5}>
+          { (userGameDetails) ? (userGameDetails.map((game, index) => {
+              return <GameCardSmall status={GetGameStatus(game.guid, userEntries)} title={game.name} imgURL={game.imgURL} deck={game.deck} key={index} guid={game.guid} />
+            })
+           ) : (<></>)
+          }
+        </Grid>
+      </Container>
+    )
   }
 
 
   return (
     <Box>
       <Header searchBarOnChange = {() => console.log("Edit Search Bar")}/>
+      {handleDisplayCards()}
     </Box>
   )
 }

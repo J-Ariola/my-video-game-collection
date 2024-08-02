1775ba706e7de2569c474ffe4af2aef185a8ab25
Jarrod Ariola <jarrod.ariola@gmail.com>
2024-08-02 09:17:47 +0900

    feat: wrap typography of title then turn to ellipses on the second line

--- src/components/GameCardSmall.tsx
+++ src/components/GameCardSmall.tsx
@@ -28,17 +28,17 @@ const GameCardSmall = (props: Props):React.JSX.Element => {
         image={imgURL}
         />
         <Box paddingX={1}>
-        <Typography variant='h4' sx={{
-          overflow: "hidden",
-          textOverflow: "ellipsis",
-          display: "-webkit-box",
-          WebkitLineClamp: "2",
-          WebkitBoxOrient: "vertical"
-        }}>{title}</Typography>
-        {/* <Typography variant='subtitle1' sx={{overflow: "hidden", textOverflow: "ellipsis", lineClamp: "2"}}>{deck}</Typography> */}
-        <Container sx={{paddingBottom: "0.5em"}}>
-          <AddToCollectionsMenu guid={guid} status={status}/>
-        </Container>
+          <Typography variant='h4' sx={{
+            overflow: "hidden",
+            textOverflow: "ellipsis",
+            display: "-webkit-box",
+            WebkitLineClamp: "2",
+            WebkitBoxOrient: "vertical"
+          }}>{title}</Typography>
+          {/* <Typography variant='subtitle1' sx={{overflow: "hidden", textOverflow: "ellipsis", lineClamp: "2"}}>{deck}</Typography> */}
+          <Container sx={{paddingBottom: "0.5em"}}>
+            <AddToCollectionsMenu guid={guid} status={status}/>
+          </Container>
         </Box>
       </Paper>
     </Grid>

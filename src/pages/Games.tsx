0366a5878e73e6b5d37f52d91d62d645ecfc6d3b
Jarrod Ariola <jarrod.ariola@gmail.com>
2024-08-07 13:50:18 +0900

    style: implement initial layout of for /games/:guid

--- src/pages/Games.tsx
+++ src/pages/Games.tsx
@@ -1,4 +1,4 @@
-import { Box, Container, Typography } from "@mui/material";
+import { Box, Grid, Typography } from "@mui/material";
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { GameDetails } from "../globals";
@@ -21,12 +21,16 @@ function Games():React.JSX.Element {
   return (
     <Box>
       {gameDetails ? 
-      <Container>
+      <Grid container>
         <Typography variant="h3">{gameDetails.name}</Typography> 
-        <img src={gameDetails.imgURL ? gameDetails.imgURL : ""}/>
-        <Typography variant="body1">{gameDetails.deck}</Typography>
-        <Typography variant="body2">{new Date(gameDetails.original_release_date as unknown as string).toISOString().split("T")[0]}</Typography>
-      </Container>:
+          <Box sx={{display: "flex"}}>
+            <Box sx={{display: "flex", width: "14rem", overflow: "hidden"}}>
+              <img style={{width: "100%"}} src={gameDetails.imgURL ? gameDetails.imgURL : ""}/>
+            </Box>
+            <Typography variant="body1" noWrap={false}>{gameDetails.deck}</Typography>
+            <Typography variant="body2">{new Date(gameDetails.original_release_date as unknown as string).toISOString().split("T")[0]}</Typography>
+          </Box>
+      </Grid>:
       <></>}
     </Box>
   );
